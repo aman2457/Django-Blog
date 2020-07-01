e14a043cf454121ce28b332c84871415088e0bf1
aman2457 <amanverm2457@gmail.com>
2020-07-01 22:03:48 +0530

    fix0005-added email name field in signup form

--- accounts/views.py
+++ accounts/views.py
@@ -5,18 +5,20 @@ from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
 from django.contrib import messages
+from .forms import SignUpForm
+
 
 def signup_view(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = SignUpForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('post:post_draft_list')
     else:        
-        form =UserCreationForm()
+        form =SignUpForm()
     return render(request,'accounts/signup.html',{'form':form})
 
 class SignUp(generic.CreateView):
-    form_class = UserCreationForm
+    form_class = SignUpForm
     success_url = reverse_lazy('login')
     template_name = 'signup.html'
