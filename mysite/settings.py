e94f47ead9a905e05794cfef42a7902b3ad53a3a
aman2457 <amanverm2457@gmail.com>
2020-07-04 21:46:16 +0530

    added password reset using email

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '4tj^^^9)w1ud)#(2_%k_364srrp79weag)qzud2%k19^961h$^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
