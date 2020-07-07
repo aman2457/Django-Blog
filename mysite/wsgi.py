9011c921c6d1c85fd6b8bb08f04597838060b8fe
aman2457 <amanverm2457@gmail.com>
2020-07-07 11:12:35 +0530

    initial commit

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -14,7 +14,7 @@ from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings.devlopment')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings.production')
 
 application = get_wsgi_application()
 
