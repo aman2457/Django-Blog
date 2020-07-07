8867cf4199697c59d2eb7379b026a757d941db22
aman2457 <amanverm2457@gmail.com>
2020-07-07 11:03:10 +0530

    initial commit

--- mysite/settings/production.py
+++ mysite/settings/production.py
@@ -6,3 +6,22 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','4tj^^^9)w1ud)#(2_%k_364srrp79we
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = False
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
+
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
+
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'aapanbihar',
+        'USER': 'name',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
+
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
