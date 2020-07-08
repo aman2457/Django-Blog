24603ac5b85e2d346b24762e2faee99e36b310f6
aman2457 <amanverm2457@gmail.com>
2020-07-09 00:11:47 +0530

    initial commit for aapanbihar

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -13,6 +13,6 @@ from django.core.wsgi import get_wsgi_application
 
 
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings.devlopment')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings.production')
 
 application = get_wsgi_application()
